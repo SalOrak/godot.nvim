31e59ac297b38f64a55481ab67916cfb69107682
SalOrak <99770300+SalOrak@users.noreply.github.com>
2026-03-16 19:08:09 +0100

    fzflua

--- lua/godot/init.lua
+++ lua/godot/init.lua
@@ -2,22 +2,66 @@ local Logger = require 'godot.logger'
 local CmdBuilder = require 'godot.command'
 local Pickers = require 'godot.picker'
 
+local M = {
+}
+
 local config = {
-	godot_exec = "godot",
+	godot_exec = nil, -- Either a Path(String), command(Table) or nil for automatic finding
 	picker = "telescope"
 }
 
-local M = {}
+local data  = {
+	last_scene = nil
+}
+
+--- Sets the godot executable. 
+---@param godot_exec String|table|nil Get the command based on the executable
+---path, command or try to find it automatically.
+local get_godot_exec = function(godot_exec)
+	if type(godot_exec) == "table" then
+		return godot_exec
+	end
+
+	local res = {}
+	if type(godot_exec) == "string" then
+		table.insert(res, godot_exec)
+	else 
+		-- TODO: maybe find more godot executables?
+		table.insert(res, "godot")
+	end
+
+	return res
+end
+
+M.last = function()
+	if not data.last_scene then
+		Logger:error("Run a scene first to rerun it later with this function.")
+		return
+	end
+
+	M.run_scene(data.last_scene)
+end
 
 --- Runs a scene from neovim
 ---@param scene String scene filepath
 M.run_scene = function(scene)
 
+	local exec = get_godot_exec(config.godot_exec)
+	vim.print(exec)
+
 	local cmd = CmdBuilder:builder()
-	cmd:add(config.godot_exec)
+
+	for _,c in pairs(exec) do
+		cmd:add(c)
+	end
 	cmd:add(scene)
 
-	vim.system(cmd:build(),{text = true}, on_exit):wait()
+	vim.print(cmd:build())
+
+	local res = vim.system(cmd:build(),{text = true}, on_exit):wait()
+	if res.code == 0 then
+		data.last_scene = scene
+	end
 end
 
 
@@ -35,7 +79,8 @@ M.run = function(opts)
 end
 
 M.setup = function(opts)
-	
+	local opts = opts or {}
+	config = vim.tbl_deep_extend('force', config, opts)
 end
 
 
--- lua/godot/pickers/_fzflua.lua
+++ lua/godot/pickers/_fzflua.lua
@@ -1,32 +1,37 @@
 local Godot = require'godot'
 local Fzf = require'fzf-lua'
+local Path = require'fzf-lua'.path
 
 -- TODO: Sane config defaults for fzf-lua 
 
 local scene_files = " .tscn$"
 
+
+local get_fullpath = function(selected, opts)
+end
+
 local defaults = {
     prompt = "[Godot] Run scene >> ",
+	cwd_prompt = false,
     actions = {
-        ["default"] = function(selected)
-			vim.print(selected)
+        ["default"] = function(selected, opts)
+			--- Inspiration from fzf_lua.actions.vimcmd_entry
+			local entry = Path.entry_to_file(selected[1], opts, opts._uri)
+			if entry.path == "<none>" then return end
+			local fullpath = entry.bufname or entry.uri and entry.uri:match("^[%a%-]+://(.*)") or entry.path
+			if not fullpath then return end
+			if not Path.is_absolute(fullpath) then
+				fullpath = Path.join({ opts.cwd or opts._cwd or vim.loop.cwd(), fullpath })
+			end
+
+			Godot.run_scene(fullpath)
 		end
     },
+	previewer = false,
 	fd_opts = "--color=never --type f --hidden --follow --exclude .git" .. scene_files,
 	find_opts = "--color=never --type f --hidden --follow --exclude .git" .. scene_files,
 	rg_opts = "--color=never --files --hidden --follow -g '!.git'" .. scene_files,
-	fn_format_entry = function(entry)
-		if entry.alias then
-			return (
-				"["
-				.. entry.alias
-				.. "] "
-				.. vim.fn.fnamemodify(entry.path, ":t")
-			)
-		end
-
-		return entry.path
-	end
+	file_icons = false,
 }
 
 
